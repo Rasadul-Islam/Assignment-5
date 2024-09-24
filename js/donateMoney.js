7a0399804010110580a1560e77d86f3447a533fa
Rasadul Islam <rasadulislam.cse@gmail.com>
2024-09-24 14:54:08 +0600

    added inner blog html page

--- js/donateMoney.js
+++ js/donateMoney.js
@@ -7,15 +7,16 @@ document.getElementById('history-btn').addEventListener('click', function(){
     changeBottonColorById('history-btn');
 })
 document.getElementById('btn-add-money').addEventListener('click', function(){
+    const accountBalance = getTextValueByID('account-balance');
     const addMoney = getInputValueByID('input-noakhali-money');
     const balanceNum = getTextValueByID('donate-amount-noakhali');
     
-    if(!isNaN(addMoney) && !isNaN(balanceNum) ){
+    
+    if(!isNaN(addMoney) && !isNaN(balanceNum) && addMoney >= 0 && accountBalance >= 0){
         const newBalance = balanceNum + addMoney;
+        const newAccountBalance = accountBalance - addMoney;
+        document.getElementById('account-balance').innerText = newAccountBalance;
         document.getElementById('donate-amount-noakhali').innerText = newBalance;
-        console.log(newBalance); 
-        // add donation history
-        
     }
     else{
         alert("Invalid Inpur: Please give a Number");
